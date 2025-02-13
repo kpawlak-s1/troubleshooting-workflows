12994ec96c2a8b950f384cbf09f86523d226a5c6
kpawlak-s1 <142378353+kpawlak-s1@users.noreply.github.com>
2025-02-12 16:44:16 -0800

    Update syslog-troubleshooting-part2.sh

--- syslog-troubleshooting-part2.sh
+++ syslog-troubleshooting-part2.sh
@@ -1,4 +1,16 @@
 # get container id of the scalyr agent container
 docker_scalyr_id=$(docker ps | grep scalyr-agent-docker-json | awk '{print $1}')
 
-docker exec -it $docker_scalyr_id /bin/bash
+#request the file parsing  status of the agent.json file in the scalyr docker container
+docker exec "$docker_scalyr_id" scalyr-agent-2 status -v | grep 'Good (files parsed successfully)' >> agent-json-parsing-status.txt
+
+parsing_check = cat ./agent-json-parsing-status-txt | grep 'Good' 
+
+# Check if "test" exists in the text
+if [[ $parsing_check == *"Good"* ]]; then
+    echo "scalyr agent config successfully parsed"
+else
+    echo "scalyr agent config file not successfully parsed. please open /etc/scalyr-agent-2/agent.json and inspect for potential issues"
+    exit 1
+fi
+
